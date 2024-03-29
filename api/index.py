89d1e4b63da3117b256ff6da26ddc4e350342d11
sneh1234 <snehgupta42@gmail.com>
2024-03-29 13:12:04 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,29 +9,29 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
 
-        dl = dataloader()
-        dl.get_stock_data('TATASTEEL')
+        # dl = dataloader()
+        # dl.get_stock_data('TATASTEEL')
 
         self.end_headers()
         self.wfile.write('Hello, world3!'.encode('utf-8'))
-        self.wfile.write(os.getcwd().encode('utf-8'))
+        self.wfile.write(os.listdir())
 
-        self.wfile.write("<img src='../plots/my_plot.png'/>")
+        # self.wfile.write("<img src='../plots/my_plot.png'/>")
         return
 
 
 
-class dataloader():
-    def __init__():
-        pass
-    def get_stock_data(stock):
-        if not os.path.exists('./plots/my_plot.png'):
+# class dataloader():
+#     def __init__():
+#         pass
+#     def get_stock_data(stock):
+#         if not os.path.exists('./plots/my_plot.png'):
                 
-            ticker = yf.Ticker('{}.NS'.format(stock))
-            aapl_df = ticker.history(period="5y")
-            plt.plot(aapl_df)
-            fig.savefig('../plots/my_plot.png')
-            plt.close()
+#             ticker = yf.Ticker('{}.NS'.format(stock))
+#             aapl_df = ticker.history(period="5y")
+#             plt.plot(aapl_df)
+#             fig.savefig('../plots/my_plot.png')
+#             plt.close()
 
 
 
