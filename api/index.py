539e95e15abdefc54724621a93a20d59d7a7dc94
sneh1234 <snehgupta42@gmail.com>
2024-03-29 07:18:52 +0000

    added plots

--- api/index.py
+++ api/index.py
@@ -1,10 +1,33 @@
 from http.server import BaseHTTPRequestHandler
 
+import yfinance as yf
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
+
+        dl = dataloader()
+        dl.get_stock_data('TATASTEEL')
+
         self.end_headers()
         self.wfile.write('Hello, world2!'.encode('utf-8'))
+
+        self.wfile.write("<img src='../plotsmy_plot.png'/>")
         return
+
+
+
+class dataloader():
+    def __init__():
+        passs
+    def get_stock_data(stock):
+        ticker = yf.Ticker('{}.NS'.format(stock))
+        aapl_df = ticker.history(period="5y")
+        fig, ax = plt.subplots()
+        s.plot.bar()
+        fig.savefig('../plots/my_plot.png')
+
+
+
